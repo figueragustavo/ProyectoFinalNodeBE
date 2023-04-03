e96c378ed42e6908917a2a3b823a7d9507968dd9
gfiguera <figueragustavo@gmail.com>
2023-04-03 20:25:30 -0300

    se modifica conexion a la base

--- app.js
+++ app.js
@@ -4,20 +4,16 @@ const {dbConnectMySql} = require("./config/mysql")
 
 const app = express();
 
-
-const NODE_ENV = process.env.NODE_ENV || 'development';
-
 app.use(express.json());
 
 const port = process.env.PORT || 3002;
 
 app.use("/api", require("./routes"));
 
-if(NODE_ENV == 'test'){
+
   app.listen(port, () => {
     console.log("http://localhost:"+port);
   });
-}
 
 dbConnectMySql();
 
